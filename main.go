524da1ce330b9cdaea3797948a23ebff2c0484b4
Allan Okoth <allanokoth695@gmail.com>
2023-05-21 01:43:33 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
 
 func main() {
 
+	// Create a new request router that will receive all requests and pass it to request handler
+	r := mux.NewRouter()
+
+	 r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    })
+
+    http.ListenAndServe(":80", r)
 }
